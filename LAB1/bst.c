ca68f49d96e39e26f9c8789140490454735acfba
Jessica Dozal <jldozalcruz@miners.utep.edu>
2017-10-10 15:52:16 -0600

    Added Makefile, bst.c, bst.o

---
+++ LAB1/bst.c
@@ -0,0 +1,9 @@
+#include <stdio.h>		/* for puts,  */
+#include <stdlib.h> 		/* for malloc */
+#include <assert.h>		/* for assert */
+#include "bst.h"		
+
+int bstDoCheck = 1;
+
+#define doCheck(_bst) (bstDoCheck)
+
