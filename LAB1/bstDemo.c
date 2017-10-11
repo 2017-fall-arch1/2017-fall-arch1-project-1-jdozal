4101baa57d28d2d7eaf375ab468d1100540aecfc
Jessica Dozal <jldozalcruz@miners.utep.edu>
2017-10-11 13:14:32 -0600

    Start interface

--- LAB1/bstDemo.c
+++ LAB1/bstDemo.c
@@ -18,18 +18,66 @@ int gets_n(char *s, int limit) {
   return(p-s);
   
 }
+
+void add(BST *bst){
+  char name[100];
+
+  printf("Enter new employee: ");
+  scanf("%s", name);
+  bstPut(bst, name);
+  printAsc(bst);
+}
 int main(){
   char buf[100];
 
   /* Make empty BST */
   BST *bst = BSTAlloc();
 
-  while(gets_n(buf, 100))
+  printf("Welcome to the Personnel Management System\n\n Enter a number to perform an action:\n  1: Add a new employee\n  2: Remove an employee\n  3: List  all employees\n\n");
+
+  printf("Option: ");
+  int option = getchar();
+
+  printf("\n");
+
+  switch (option) {
+  case '0':
+    printf("Bye");
+    return 0;
+  case '1':
+    printf("add");
+    add(bst);
+    break;
+  case '2':
+    printf("remove");
+    break;
+  case '3':
+    printAsc(bst->root);
+    break;
+  default:
+    printf("Not a valid option");
+  }
+  /* if(option =='0'){
+    printf("Tenkiubai\n");
+    return 0;
+  }
+  if(option == '1'){
+    printf("Add\n");
+  }if(option == '2'){
+    printf("Remove\n");
+  }if(option == '3'){
+    printf("List\n");
+  }else{
+    printf("Not a valid option");
+  }
+  */
+  
+  /*while(gets_n(buf, 100))
     bstPut(bst, buf);
 
   printf("Start printing list ascending order: \n");
   Employee *r = bst->root;
   printAsc(r);
-  
+  */
   return 0;
 }
