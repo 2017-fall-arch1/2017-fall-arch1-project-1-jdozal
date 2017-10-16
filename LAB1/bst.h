e99378c33892883e858c3cd902a08b5f86d914cb
Jessica Dozal <jldozalcruz@miners.utep.edu>
2017-10-16 17:39:40 -0600

    Added functions missing in bst.h

--- LAB1/bst.h
+++ LAB1/bst.h
@@ -28,6 +28,15 @@ struct Employee *search(Employee *e, char *n);
 /* removes employee */
 void removeEmployee(Employee *e, Employee *parent);
 
+/* Check if employee is a leaf in the bst */
+int isLeaf(Employee *e);
+
+/* replace parent node with newChild, used for remove fuction */
+void replaceInParent(Employee *parent, Employee *child, Employee *newChild);
+
+/* get last element on left child of given employee */
+struct Employee *getLargestLeft(Employee *e);
+  
 /* search for parent */
 struct Employee *getParent(Employee *curr, Employee *prev, char *n);
 
